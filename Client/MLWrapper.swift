1ed272f3bb56c4e4faf9311ffc7d923bdf661ee2
Artem Abramov <ar.abramov@gmail.com>
2018-05-08 08:55:51 +0300

    Hotfixes

--- Client/MLWrapper.swift
+++ Client/MLWrapper.swift
@@ -2,13 +2,13 @@ import Foundation
 import Vision
 
 class MLWrapper {
-    class func process(image: CGImage, completion: @escaping (String, VNConfidence) -> Void) {
+    class func process(image: CIImage, completion: @escaping (String, VNConfidence) -> Void) {
         guard let model = try? VNCoreMLModel(for: RecyclingModel().model) else { return }
         let request = VNCoreMLRequest(model: model) { data, _ in
             guard let results = data.results as? [VNClassificationObservation] else { return }
             guard let mainResult = results.first else { return }
             completion(mainResult.identifier, mainResult.confidence)
         }
-        try? VNImageRequestHandler(cgImage: image, options: [:]).perform([request])
+        try? VNImageRequestHandler(ciImage: image, options: [:]).perform([request])
     }
 }
